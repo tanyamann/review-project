e9bf0366f66d52a1a1d2f7cc368647b18b44db53
Tanya Mann <thombre99@protomail.com>
2022-03-27 19:07:00 -0600

    add script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // Start your code ***HERE*** =========
 
 // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '#', '$', '&', '(', ')', '*', '+']
 
 // create a global variable called "pwLength" with a number between 10 and 18
+const pwLength = 11
 
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+
+function addNewPassword() {
+    let result = pwLength;
+       const charsLength = chars.length;
+       
+     for (let i = 0; i < chars.length; i++ ) {
+          result = chars[Math.floor(Math.random() * chars.length)];
+       
+          return result;
+       }
+   };
+   
+   console.log(addNewPassword());
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
